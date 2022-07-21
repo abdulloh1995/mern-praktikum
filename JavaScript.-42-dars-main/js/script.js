8606a1af17e751fada6efcdbc3ee4aa0e87cb1ab
Abdulloh <developer.sobirov4448@gmail.com>
2022-07-21 14:52:37 +0300

    js lessin 42

---
+++ JavaScript.-42-dars-main/js/script.js
@@ -0,0 +1,20 @@
+const box = document.querySelector('.box'),
+  btn = document.querySelector('button')
+
+// const width = box.clientWidth
+// const height = box.clientHeight
+
+// const width = box.offsetWidth
+// const height = box.offsetHeight
+
+// const height = box.scrollHeight
+
+// console.log(height);
+// // console.log(height);
+
+// btn.addEventListener('click', () => {
+//   box.style.height = box.scrollHeight + 'px'
+// })
+
+
+console.log(document.documentElement.clientWidth);
