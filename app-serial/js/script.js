85c3b98b2b8e0ebfbdc4f5fc6a4a98b9364c54c4
Abdulloh <developer.sobirov4448@gmail.com>
2022-08-24 17:20:44 +0300

    async

--- app-serial/js/script.js
+++ app-serial/js/script.js
@@ -104,8 +104,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
   // Modal
   const modalTrigger = document.querySelectorAll('[data-modal]'),
-    modal = document.querySelector('.modal'),
-    modalCloseBtn = document.querySelector('[data-close]')
+    modal = document.querySelector('.modal')
 
   function closeModal() {
     modal.classList.add('hide')
@@ -124,10 +123,8 @@ window.addEventListener('DOMContentLoaded', () => {
     item.addEventListener('click', openModal)
   })
 
-  modalCloseBtn.addEventListener('click', closeModal)
-
   modal.addEventListener('click', (e) => {
-    if (e.target == modal) {
+    if (e.target == modal || e.target.getAttribute('data-close') == '') {
       closeModal()
     }
   })
@@ -228,56 +225,79 @@ window.addEventListener('DOMContentLoaded', () => {
   const forms = document.querySelectorAll('form')
 
   forms.forEach((form) => {
-    postData(form)
+    bindPostData(form)
   })
 
   const msg = {
-    loading: 'Loading...',
+    loading: 'img/spinner.svg',
     success: "Thank's for submitting our form",
     failure: 'Something went wrong',
   }
 
-  function postData(form) {
+  async function postData(url, data) {
+    const res = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: data,
+    })
+
+    return await res.json()
+  }
+
+  function bindPostData(form) {
     form.addEventListener('submit', (e) => {
       e.preventDefault()
 
-      const statusMessage = document.createElement('div')
-      statusMessage.textContent = msg.loading
-      form.append(statusMessage)
+      const statusMessage = document.createElement('img')
+      statusMessage.src = msg.loading
+      statusMessage.style.cssText = `
+        display: block;
+        margin: 0 auto;
+      `
+      form.insertAdjacentElement('afterend', statusMessage)
 
-      const request = new XMLHttpRequest()
-      request.open('POST', 'server.php')
-
-      request.setRequestHeader('Content-Type', 'application/json')
-
-      const obj = {}
       const formData = new FormData(form)
 
-      formData.forEach((val, key) => {
-        obj[key] = val
-      })
+      const json = JSON.stringify(Object.fromEntries(formData.entries()))
 
-      const json = JSON.stringify(obj)
-
-      request.send(json)
-
-      request.addEventListener('load', () => {
-        if (request.status === 200) {
-          console.log(request.response)
-          statusMessage.textContent = msg.success
+      postData('http://localhost:3000/request', json)
+        .then((data) => {
+          console.log(data)
+          showThanksModal(msg.success)
+          statusMessage.remove()
+        })
+        .catch(() => {
+          showThanksModal(msg.failure)
+        })
+        .finally(() => {
           form.reset()
-          setTimeout(() => {
-            statusMessage.remove()
-          }, 2000)
-        } else {
-          statusMessage.textContent = msg.failure
-        }
-      })
+        })
     })
   }
 
-  fetch('http://localhost:3000/menu')
-  .then((data) => data.json())
-  .then((res) => console.log(res))
-  // asd
+  function showThanksModal(message) {
+    const prevModalDialog = document.querySelector('.modal__dialog')
+
+    prevModalDialog.classList.add('hide')
+    openModal()
+
+    const thanksModal = document.createElement('div')
+    thanksModal.classList.add('modal__dialog')
+    thanksModal.innerHTML = `
+      <div class="modal__content">
+        <div data-close class="modal__close">&times;</div>
+        <div class="modal__title">${message}</div>
+      </div>
+    `
+
+    document.querySelector('.modal').append(thanksModal)
+    setTimeout(() => {
+      thanksModal.remove()
+      prevModalDialog.classList.add('show')
+      prevModalDialog.classList.remove('hide')
+      closeModal()
+    }, 4000)
+  }
 })
